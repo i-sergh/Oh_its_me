e06f8710b005de578ef15223eabb9decc5ef0ea3
i-sergh <seripp28@gmail.com>
2022-09-03 16:42:12 +0300

    now we have db

--- base/views.py
+++ base/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from random import randint
-
-rooms = [
+from .models import Room
+""" rooms = [
     {'id':0, 'name':'rrr'},
     {'id':1, 'name':'ADjaCka'},
     {'id':2, 'name':'ka'},
     {'id':3, 'name':'Diskeksik'},
-]
+] """
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
+
+    print(rooms, len(rooms))
     context={'rooms':rooms}
     return render(request, 'base/home.html',context )
 def room(request, pk):
-    
-    context={'room': rooms[int(pk)]['name']  }
+    room = Room.objects.get(id=pk)
+    context={'room': room  }
     print(context)
     return render(request, 'base/room.html', context)
 
 def random_room(request):
-
-    return redirect('room', randint(0,len(rooms)-1))
+    rooms = Room.objects.all()
+    return redirect('room', randint(1,len(rooms)))
 
